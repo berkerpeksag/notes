f054073c2a078664ce4787f2be539e450b7fbab4
Berker Peksag <berker.peksag@gmail.com>
2016-09-11 05:39:23 +0300

    Simplify Dict.__getitem__

--- python/dict_getattr.py
+++ python/dict_getattr.py
@@ -1,7 +1,5 @@
 class Dict(dict):
-    def __getattr__(self, key):
-        return self.get(key)
-
+    __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
@@ -14,6 +12,7 @@ def main():
     print(e.c)
     del e.b
     print('b' not in d)
+    print(e.d)  # will raise a KeyError
 
 if __name__ == '__main__':
     main()
